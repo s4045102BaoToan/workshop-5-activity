c7a8a13a5f7d68b5e0304ebf9bdd4fb7a200a083
Quy Duc <s4070049@rmit.edu.vn>
2024-05-27 01:49:27 +0700

    1

--- src/main/java/app/PeriodSimilar.java
+++ src/main/java/app/PeriodSimilar.java
@@ -179,7 +179,7 @@ String startYear = context.formParam("startingYear");
 String yearLength = context.formParam("yearLength");
 String x = context.formParam("x");
 String type = context.formParam("valueType");
-
+String push;
 html += """
 </div>      
       <!--------------------------------------------------out put--------------------------------------------------->
